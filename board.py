1b65d95c90a3d04fbe7724be68a3d6410da7c48d
Adrian S <67870240+adinsoon@users.noreply.github.com>
2020-07-11 00:19:09 +0200

    Second commit: - Class TextView added: - Small improvements - New methods related to board

--- board.py
+++ board.py
@@ -1,7 +1,6 @@
 import cell
-from enum import Enum
 from random import randint
-from array import *
+from enum import Enum
 
 
 class GameMode(Enum):
@@ -14,34 +13,52 @@ class Board:  # represents a field of cells
 
     # board has following attributes:
     # size of height and width (row, col)
-    # potentially a game mode which sets the number of bombs on the board
+    # game modes which set the number of bombs on the board
     # board will have following action methods:
+    # (utility) getters
     # (user) toggle flag
     # (user) reveal field
-    # (utility) getters
     # (utility) count neighbours -> then set value for each cell
     # (utility) display board in debug mode (everything revealed)
 
     def __init__(self, height, width, mode):
-        self.cells = []
+        self.cells = [[cell.Cell(i, j) for i in range(width)] for j in range(height)]
         self.resolution = [height, width]
         self.mode = mode
         self.bombs_quantity = height * width
 
-        for x in range(height):
-            for y in range(width):
-                self.cells.append(cell.Cell(x, y))
+    def get_board_height(self):
+        return self.resolution[0]
+
+    def get_board_width(self):
+        return self.resolution[1]
+
+    def get_field_info(self, height, width):
+        if not self.cells[height][width].is_revealed() and self.cells[height][width].has_flag():
+            return 'F'
+        elif not self.cells[height][width].is_revealed() and not self.cells[height][width].has_flag():
+            return '_'
+        elif self.cells[height][width].is_revealed() and self.cells[height][width].has_bomb():
+            return 'x'
+        elif self.cells[height][width].is_revealed():
+            return ' '
 
     def debug_display(self):
-        for i in range(len(self.cells)):
-            if i > 0 and i % self.resolution[1] == 0:
-                print()
-            if not self.cells[i].is_revealed():
-                print("-", end=" ")
-            elif self.cells[i].has_bomb():
-                print("X", end=" ")
-            elif self.cells[i].has_flag():
-                print("F", end=" ")
+        for i in range(self.get_board_height()):
+            for j in range(self.get_board_width()):
+                if self.cells[i][j].has_bomb():
+                    print("M", end=" ")
+                else:
+                    print(".", end=" ")
+                if self.cells[i][j].is_revealed():
+                    print("o", end=" ")
+                else:
+                    print(".", end=" ")
+                if self.cells[i][j].has_flag():
+                    print("f  ", end=" ")
+                else:
+                    print(".  ", end=" ")
+            print()
 
     def set_mode(self):
         if self.mode == GameMode.EASY.name:
@@ -52,3 +69,12 @@ class Board:  # represents a field of cells
 
         elif self.mode == GameMode.HARD.name:
             self.bombs_quantity = int(self.bombs_quantity * 0.3)
+
+    def get_positions(self):
+        for i in range(self.resolution[0]):
+            for j in range(self.resolution[1]):
+                print(self.cells[i][j].get_position(), end=" ")
+            print()
+
+
+
